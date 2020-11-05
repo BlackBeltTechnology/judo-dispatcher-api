4191edb7f793553be319c1c8a91daad7e0be3545
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2020-11-05 21:23:03 +0100

    JNG-1252: rename interface

--- src/main/java/hu/blackbelt/judo/dispatcher/api/VariableResolver.java
+++ src/main/java/hu/blackbelt/judo/dispatcher/api/VariableResolver.java
@@ -5,7 +5,7 @@ import java.util.Map;
 /**
  * Environment variable resolver.
  */
-public interface EnvironmentVariableResolver {
+public interface VariableResolver {
 
     <T> T resolve(Class<T> type, String category, String key, Map<String, Object> context);
 }
