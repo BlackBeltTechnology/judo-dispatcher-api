cc7ea1b4f014a3c83471d19aeb26e0ff95601088
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2021-09-21 12:43:33 +0200

    JNG-2510: cleanup

--- src/main/java/hu/blackbelt/judo/dispatcher/api/FileType.java
+++ src/main/java/hu/blackbelt/judo/dispatcher/api/FileType.java
@@ -1,67 +1,15 @@
 package hu.blackbelt.judo.dispatcher.api;
 
-import com.google.gson.Gson;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Collections;
-import java.util.Map;
-
-import static java.util.Objects.requireNonNull;
-
+@Getter
+@Builder
 public class FileType {
 
-    private String stringValue;
+    private String id;
+    private String fileName;
+    private Long size;
+    private String mimeType;
 
-    private Map<String, Object> data;
-
-    public FileType(final String stringValue) {
-        requireNonNull(stringValue, "Missing value");
-        this.stringValue = stringValue;
-    }
-
-    public FileType(final Map<String, Object> value) {
-        data = value;
-    }
-
-    public Map<String, Object> getData() {
-        if (data == null) {
-            data = new Gson().fromJson(stringValue, Map.class);
-        }
-        return Collections.unmodifiableMap(data);
-    }
-
-    public String getId() {
-        return String.valueOf(getData().get(Part.FILE_ID.getKey()));
-    }
-
-    public String getFileName() {
-        return (String) getData().get(Part.FILE_NAME.getKey());
-    }
-
-    public Long getSize() {
-        final Object size = getData().get(Part.FILE_SIZE.getKey());
-        if (size == null || size instanceof Long) {
-            return (Long) size;
-        } else {
-            return Double.valueOf(size.toString()).longValue();
-        }
-    }
-
-    public String getMimeType() {
-        return (String) getData().get(Part.FILE_MIME_TYPE.getKey());
-    }
-
-    @Getter
-    @AllArgsConstructor
-    public enum Part {
-
-        FILE_ID("sub"),
-        FILE_NAME("fileName"),
-        FILE_SIZE("fileSize"),
-        FILE_MIME_TYPE("mimeType"),
-        CONTEXT("ctx");
-
-        private String key;
-    }
 }
